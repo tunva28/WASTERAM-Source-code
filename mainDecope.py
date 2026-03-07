4e6955a02c7e68a1c3124634ab48b4bd6c6803e6
Breamm <62603048+tunva28@users.noreply.github.com>
2026-03-07 17:48:48 +0700

    Add files via upload

---
+++ mainDecope.py
@@ -0,0 +1,161 @@
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: 'main.py'
+# Bytecode version: 3.13.0rc3 (3571)
+# Source timestamp: 1970-01-01 00:00:00 UTC (0)
+
+# ***<module>: Failure: Different control flow
+import time
+import colorama
+import os
+from colorama import Fore, Back, Style
+colorama.init()
+import sys
+import webbrowser as WEB
+spiner = ['|', '/', '-', '\\']
+version = 'indev build 532026 3'
+BradarURL = 'https://youtube.com/shorts/gTyaRoxySrE?si=ps7kEeQ1QtQjylpm'
+KimURL = 'https://www.youtube.com/watch?v=7maE0J9S-T0&list=RD7maE0J9S-T0&start_radio=1'
+ABATAURL = 'https://www.youtube.com/watch?v=EzQiEOQ8nVA&list=RD7maE0J9S-T0&index=4'
+Movie1 = 'https://youtu.be/JreSPMu_VGY?si=zjh5ss8bQCIwix82'
+Peak1 = 'https://www.youtube.com/watch?v=7fzMxxyklTw&list=PLvNp0Boas7205K42TtDtdUCCrtkD-X0id&index=1'
+scream = 'https://www.youtube.com/watch?v=ykGB4zu7gTc'
+fun = 'https://www.youtube.com/watch?v=GtL1huin9EE&list=RDGtL1huin9EE&start_radio=1'
+yell = 'https://www.youtube.com/watch?v=-5R_sviyV4Y'
+peakOSURL = 'https://www.youtube.com/watch?v=hmjU-6tkEc8'
+burgerURL = 'https://www.youtube.com/shorts/SxtsnWjD_UA'
+def intro():
+    print(f'{Fore.CYAN}Welcome to')
+    print(f'{Fore.RED}\n █████   ███   █████   █████████    █████████  ███████████ ██████████ ███████████     █████████   ██████   ██████\n░░███   ░███  ░░███   ███░░░░░███  ███░░░░░███░█░░░███░░░█░░███░░░░░█░░███░░░░░███   ███░░░░░███ ░░██████ ██████ \n ░███   ░███   ░███  ░███    ░███ ░███    ░░░ ░   ░███  ░  ░███  █ ░  ░███    ░███  ░███    ░███  ░███░█████░███ \n ░███   ░███   ░███  ░███████████ ░░█████████     ░███     ░██████    ░██████████   ░███████████  ░███░░███ ░███ \n ░░███  █████  ███   ░███░░░░░███  ░░░░░░░░███    ░███     ░███░░█    ░███░░░░░███  ░███░░░░░███  ░███ ░░░  ░███ \n  ░░░█████░█████░    ░███    ░███  ███    ░███    ░███     ░███ ░   █ ░███    ░███  ░███    ░███  ░███      ░███ \n    ░░███ ░░███      █████   █████░░█████████     █████    ██████████ █████   █████ █████   █████ █████     █████\n     ░░░   ░░░      ░░░░░   ░░░░░  ░░░░░░░░░     ░░░░░    ░░░░░░░░░░ ░░░░░   ░░░░░ ░░░░░   ░░░░░ ░░░░░     ░░░░░ \n                                                                                                                 \n         stupid Game\n   ')
+    print(f'{Fore.YELLOW}(version:{version})')
+    print(f'{Fore.LIGHTGREEN_EX}By -> Breamm aka Tunva <-')
+    print()
+    print('A completely useless terminal program.')
+    print()
+def loading():
+    # ***<module>.loading: Failure: Different bytecode
+    for i in range(20):
+        sys.stdout.flush()
+        time.sleep(0.2)
+    print(f'\nDone {Fore.CYAN}Bradar!')
+def clearT():
+    os.system('cls' if os.name == 'nt' else 'clear')
+def peakOS():
+    print(f'{Fore.LIGHTYELLOW_EX}\n ███████████                                    ████                 ███████     █████████ \n░█░░░███░░░█                                   ░░███               ███░░░░░███  ███░░░░░███\n░   ░███  ░   ██████  █████████████   ████████  ░███   ██████     ███     ░░███░███    ░░░ \n    ░███     ███░░███░░███░░███░░███ ░░███░░███ ░███  ███░░███   ░███      ░███░░█████████ \n    ░███    ░███████  ░███ ░███ ░███  ░███ ░███ ░███ ░███████    ░███      ░███ ░░░░░░░░███\n    ░███    ░███░░░   ░███ ░███ ░███  ░███ ░███ ░███ ░███░░░     ░░███     ███  ███    ░███\n    █████   ░░██████  █████░███ █████ ░███████  █████░░██████     ░░░███████░  ░░█████████ \n   ░░░░░     ░░░░░░  ░░░░░ ░░░ ░░░░░  ░███░░░  ░░░░░  ░░░░░░        ░░░░░░░     ░░░░░░░░░  \n                                      ░███                                                 \n                                      █████                                                \n                                     ░░░░░                                                 \n         {Fore.YELLOW}R.I.P Terry A. Davis\n   ')
+def listCMS():
+    print(f'{Fore.LIGHTYELLOW_EX}-------------------------------')
+    print('==>>here is the command list<<==')
+    print()
+    print('> atsp >about this useless program?<')
+    print('> hello')
+    print('> clear')
+    print('> exit')
+    print('> test')
+    print('> what 9+10')
+    print('> Bradar')
+    print('> kim jong un')
+    print('> SONIC CD')
+    print('> squidward yell')
+    print('> burger')
+    print()
+    print('==>>Silent Movie Zone 1920<<==')
+    print()
+    print('> smo 1')
+    print()
+    print('==>>command zone<<==')
+    print()
+    print('> command A')
+    print('> command C')
+    print('> command R')
+    print('> command print')
+    print('> command load')
+    print('> command scream')
+    print()
+    print('==>>Holy command ZONE<<==')
+    print()
+    print('> Holy OS?')
+    print('-------------------------------')
+clearT()
+intro()
+print('say Enter command for list of command bardar')
+print(f'{Fore.LIGHTYELLOW_EX}^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+while True:
+    GETSOME = input(f'{Fore.GREEN}hello bradar Plz Enter something bradar=>')
+    if GETSOME == 'command':
+        listCMS()
+    else:
+        if GETSOME == 'atsp':
+            clearT()
+            intro()
+        else:
+            if GETSOME == 'hello':
+                print(f'{Fore.LIGHTGREEN_EX}Hello World bradar.')
+            else:
+                if GETSOME == 'clear':
+                    clearT()
+                else:
+                    if GETSOME == 'test':
+                        print(f'{Fore.LIGHTGREEN_EX}test')
+                        loading()
+                        print('the test is DONE bradar!')
+                    else:
+                        if GETSOME == 'command print':
+                            print('print text?')
+                            inputthing = input('INPUT : Text?-->')
+                            print(f'{Fore.BLUE}print:{Fore.YELLOW}{inputthing}')
+                        else:
+                            if GETSOME == 'command C':
+                                print('wat is ts command bradar delete it.')
+                            else:
+                                if GETSOME == 'what 9+10':
+                                    print('21 bradar')
+                                else:
+                                    if GETSOME == 'Bradar':
+                                        loading()
+                                        WEB.open_new_tab(BradarURL)
+                                    else:
+                                        if GETSOME == 'command A':
+                                            loading()
+                                            WEB.open_new_tab(ABATAURL)
+                                        else:
+                                            if GETSOME == 'command load':
+                                                loading()
+                                            else:
+                                                if GETSOME == 'exit':
+                                                    # return None
+                                                    pass
+                                                else:
+                                                    if GETSOME == 'kim jong un':
+                                                        loading()
+                                                        WEB.open_new_tab(KimURL)
+                                                    else:
+                                                        if GETSOME == 'SONIC CD':
+                                                            loading()
+                                                            WEB.open_new_tab(Peak1)
+                                                        else:
+                                                            if GETSOME == 'smo 1':
+                                                                loading()
+                                                                WEB.open_new_tab(Movie1)
+                                                            else:
+                                                                if GETSOME == 'command scream':
+                                                                    loading()
+                                                                    WEB.open_new_tab(scream)
+                                                                else:
+                                                                    if GETSOME == 'command R':
+                                                                        loading()
+                                                                        WEB.open_new_tab(fun)
+                                                                    else:
+                                                                        if GETSOME == 'squidward yell':
+                                                                            loading()
+                                                                            WEB.open_new_tab(yell)
+                                                                        else:
+                                                                            if GETSOME == 'burger':
+                                                                                loading()
+                                                                                WEB.open_new_tab(burgerURL)
+                                                                            else:
+                                                                                if GETSOME == 'Holy OS?':
+                                                                                    loading()
+                                                                                    peakOS()
+                                                                                    WEB.open_new_tab(peakOSURL)
+                                                                                else:
+                                                                                    print(f'{Fore.RED}Bradar What is this command Bradar')
+                                                                                    print(f'{Fore.RED}say Enter command for list of command bardar')
